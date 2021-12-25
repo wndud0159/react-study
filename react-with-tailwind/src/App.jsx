869c7836994b257d7516f002754569c54c432402
wndud0159 <wndud0159@naver.com>
2021-12-25 20:58:09 +0900

    react cra with tailwind added

---
+++ react-with-tailwind/src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="App">
+      <div className=" border text-center py-3 space-y-3">
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+        <div className=" text-lime-300 text-3xl font-bold">green color</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
